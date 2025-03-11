5c5d1b8dd125b90c907117b9eb976ae390646bb7
JWMin556 <117150131+JWMin556@users.noreply.github.com>
2025-03-11 23:02:56 +0900

    예습 및 3월 11일 실습본 싹다 올림

---
+++ 02_Vue/01_Node/SRC/chapter02/sec01/func01.js
@@ -0,0 +1,4 @@
+function getTriangle(base, height) {
+    return (base * height) / 2;
+}
+console.log('삼각형의면적:' + getTriangle(5, 2));
---
+++ 02_Vue/01_Node/SRC/chapter02/sec03/user.js
@@ -0,0 +1,2 @@
+const user = '주머니';
+module.exports = user; // user 변수내보내기
